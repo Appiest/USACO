c74abb5685e65b2a2865f7f18b9c081248b76a3d
Appiest <90289022+Appiest@users.noreply.github.com>
2022-01-09 09:21:07 -0800

    Update MooBuzz.cpp

--- Silver/2019/December/MooBuzz.cpp
+++ Silver/2019/December/MooBuzz.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	freopen("moobuzz.in", "r", stdin);
+	freopen("moobuzz.out", "w", stdout);
+
+	int n; cin >> n;
+
+	int res = 0;
+	while (n - 8 > 0) {
+		res += 15;
+		n -= 8;
+	}
+
+	int plus[8] = {1, 2, 4, 7, 8, 11, 13, 14};
+
+	cout << res + plus[n - 1] << '\n';
+}
 
